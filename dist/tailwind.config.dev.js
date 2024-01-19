83ab9f6be7b08ed109782a49bece288a7133bfa2
SAJID AHMAD HAJAM <sajidahmadhajam@gmail.com>
2024-01-19 23:28:36 +0530

    first commit

---
+++ dist/tailwind.config.dev.js
@@ -0,0 +1,16 @@
+"use strict";
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {}
+  },
+  plugins: [],
+  fontFamily: {
+    Whitney: ["Whitney"],
+    Ginto: ["Ginto"],
+    ggSans: ["ggSans"]
+  }
+};
+//# sourceMappingURL=tailwind.config.dev.js.map
